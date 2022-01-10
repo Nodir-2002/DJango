5676df2421f28635561e4fc3e5b824377b34e8c7
Nodir-2002 <81639350+Nodir-2002@users.noreply.github.com>
2022-01-10 10:32:56 +0300

    Test gacha

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+class PostModelTest(TestCase):
+    def setup(self):
+        Post.objects.create(title='Havo',text='bugun havo ochiq')
+
+    def text_test(self):
+        post=Post.objects.get(id=1)
+        Ttitle =f"{post.title}"
+        Ttext = f"{post.text}"
+        self.assertEqual(Ttitle,'Havo')
+        self.assertEqual(Ttext,'bugun havo ochiq')
+
